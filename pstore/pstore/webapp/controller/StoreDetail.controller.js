a65cffeab142b6390f4df0e1eeb8327fc51813cb
liang xiaorui <lxiaor@gmail.com>
2022-10-11 06:12:10 +0000

    Add Section 9

--- pstore/pstore/webapp/controller/StoreDetail.controller.js
+++ pstore/pstore/webapp/controller/StoreDetail.controller.js
@@ -10,23 +10,46 @@ sap.ui.define([
 
         return Controller.extend("com.shin.pstore.pstore.controller.StoreDetail", {
             onInit: function () {
-                if(!this._viewState){
+                if (!this._viewState) {
                     this._viewState = {};
                 }
 
                 this._viewState.editable = true;
-                
+
                 var viewStateModel = new sap.ui.model.json.JSONModel(this._viewState);
                 this.getView().setModel(viewStateModel, "viewState");
-                
+
+                var page = this.getView().byId("objectPageLayout");
+                page.setSelectedSection("section1");
+
                 this._oRouter = UIComponent.getRouterFor(this);
                 this._oRouter.getRoute("StoreDetail").attachPatternMatched(this._onDetailMatched, this);
             },
 
             _onDetailMatched: function (oEvent) {
                 var sPath = "/DataSet/" + oEvent.getParameter("arguments").id;
-                var oView = this.getView(); 
+                var oView = this.getView();
                 oView.bindElement(sPath);
             },
+
+            onSave: function () {
+
+            },
+
+            onChange: function () {
+
+            },
+
+            onRequest: function () {
+
+            },
+
+            onGenerate: function () {
+
+            },
+
+            onStop: function () {
+
+            }
         });
     });
