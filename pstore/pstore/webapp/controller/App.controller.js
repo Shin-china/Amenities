344e6830ede7ff3457fce0c6d1ec2d7a7ca4fb00
liangxiaorui <815142908@qq.com>
2022-10-23 08:29:59 +0800

    Add OData Binding

--- pstore/pstore/webapp/controller/App.controller.js
+++ pstore/pstore/webapp/controller/App.controller.js
@@ -5,7 +5,7 @@ sap.ui.define(
     function(BaseController) {
       "use strict";
   
-      return BaseController.extend("com.shin.pstore.pstore.controller.controller.App", {
+      return BaseController.extend("com.shin.pstore.pstore.controller.App", {
         onInit() {
         }
       });
