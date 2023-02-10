681ce3738235610c100fa8fe57f3fa195ed964c9
P@assw0rd123! <stanley.shi@sh.shin-china.com>
2023-02-10 10:10:25 +0800

    提交20230210

--- pstore004/webapp/controller/DailyBalance.controller.js
+++ pstore004/webapp/controller/DailyBalance.controller.js
@@ -1406,6 +1406,65 @@ sap.ui.define([
             }
         },
 
+        onPrintPDF: function (oEvent) {
+            // var sAccount = oEvent.getParameter("value"); 
+            var sKAISHA_CD = this._LocalData.getProperty("/dailyBalance/0/KAISHA_CD");
+            var sKIHYO_NO = this._LocalData.getProperty("/dailyBalance/0/KIHYO_NO");
+            var sTENPO_CD = this._LocalData.getProperty("/dailyBalance/0/TENPO_CD");
+            var sEIGYO_BI = this._LocalData.getProperty("/dailyBalance/0/EIGYO_BI");
+            var sNIKKEIHYO_STATUS = this._LocalData.getProperty("/dailyBalance/0/NIKKEIHYO_STATUS");
+
+            var sUrl = "/sap/opu/odata/sap/ZZPSTORE003_SRV/ZzExportSet(KAISHA_CD='" + sKAISHA_CD + "',KIHYO_NO='" + sKIHYO_NO + "')/$value";
+            // window.open(sUrl, "_blank");
+            var sShop = sTENPO_CD;
+            if (sShop.length == 3) {
+                sShop = "0" + sShop;
+            }
+            var sFielName = "Amenities_本部_" + sShop + "_" + sEIGYO_BI + sNIKKEIHYO_STATUS;
+            this.download(sUrl, sFielName);　
+        },
+
+        getBlob: function (url) {
+            return new Promise(resolve => {
+              const xhr = new XMLHttpRequest();
+        
+              xhr.open('GET', url, true);
+              xhr.responseType = 'blob';
+              xhr.onload = () => {
+                if (xhr.status === 200) {
+                  resolve(xhr.response);
+                }
+              };
+        
+              xhr.send();
+            });
+        },
+        saveAs: function (blob, filename) {
+            if (window.navigator.msSaveOrOpenBlob) {
+              navigator.msSaveBlob(blob, filename);
+            } else {
+              const link = document.createElement('a');
+              const body = document.querySelector('body');
+        
+              link.href = window.URL.createObjectURL(blob);
+              link.download = filename;
+        
+              // fix Firefox
+              link.style.display = 'none';
+              body.appendChild(link);
+        
+              link.click();
+              body.removeChild(link);
+        
+              window.URL.revokeObjectURL(link.href);
+            }
+        },
+        download: function (url, sFielName) {
+            this.getBlob(url).then(blob => {
+              this.saveAs(blob, sFielName);
+            });
+        },
+
         controlFieldEnabled: function () {
             var aTableColumn = [
                 "KUAS_SAKUHINMEI",
